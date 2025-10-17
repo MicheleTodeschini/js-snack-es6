0fa0944a02112510b71ec87a07b9882c2916015d
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-10-17 13:57:31 +0200

    first commit

---
+++ snack1/main.js
@@ -0,0 +1,28 @@
+console.log('funzia');
+
+
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
+
+//creo array di oggetti
+//assegno nome e peso
+//stampo bici con peso minore
+
+const biciEl = document.getElementById('bici')
+
+const biciclette = [{
+    nome: 'Bianchi',
+    peso: 10
+},
+{
+    nome: 'Pinarello',
+    peso: 8
+},
+{
+    nome: 'Colnago',
+    peso: 9
+}
+]
+
+console.log(biciclette);
