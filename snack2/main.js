d5eecf971e3cb4896f2f9851bc047aae4d8e76f7
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-10-17 15:02:18 +0200

    add == to line 31 so now i can see the points in the console

--- snack2/main.js
+++ snack2/main.js
@@ -28,7 +28,7 @@ const squadre = [
 
 
 for (let i = 0; i < squadre.length; i++) {
-    if (squadre[i].punti = 0) {
+    if (squadre[i].punti == 0) {
         squadre[i].punti = Math.floor(Math.random() * 100);
         console.log(squadre[i]);
 
