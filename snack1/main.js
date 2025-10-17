82d74a3c5941834112b8e39d9f890f6375e56007
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-10-17 15:45:35 +0200

    changed i=0 to i=1 in es 1 line 31 col14

--- snack1/main.js
+++ snack1/main.js
@@ -28,7 +28,7 @@ const biciclette = [{
 
 let biciLeggera = biciclette[0]
 
-for (let i = 0; i < biciclette.length; i++)
+for (let i = 1; i < biciclette.length; i++)
     if (biciclette[i].peso < biciLeggera.peso) {
         biciLeggera = biciclette[i]
         biciEl.innerHTML = `la più leggera è ${biciLeggera}`
